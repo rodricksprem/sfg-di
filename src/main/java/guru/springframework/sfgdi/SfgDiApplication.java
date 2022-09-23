f960b71eaabdeece75c4f86f74aae5cfd76c39b9
rodricks <rodricks.premkumar@biogen.com>
2022-09-23 05:38:40 +0530

    Assignment submitted

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -17,6 +17,9 @@ org.springframework.beans.factory.BeanDefinitionStoreException: Failed to parse 
 	 */
 	public static void main(String[] args) {
 	ApplicationContext ctx= SpringApplication.run(SfgDiApplication.class, args);
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
 		I18nController i18nController =(I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
 	MyController myController =(MyController) ctx.getBean("myController");
